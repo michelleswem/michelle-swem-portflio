6fea510d1a90873ff5f1331d1b3c2e24efad9442
michelleswem <swemmichelle990@gmail.com>
2023-04-13 14:34:26 +0100

    fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ const MyApp = ({ Component, pageProps }) => {
     <Layout>
       <Head>
         <meta charSet="UTF-8" />
-        <meta http-Equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="description"
